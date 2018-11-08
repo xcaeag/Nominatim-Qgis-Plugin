8f500699788d770c02a9a7c81b1ff100a4013f59
CULOS <xavier.culos@eau-adour-garonne.fr>
2018-11-08 17:07:31 +0100

    use of showPluginHelp

--- nominatim/nominatim.py
+++ nominatim/nominatim.py
@@ -21,14 +21,15 @@ import os
 
 from PyQt5.QtCore import (QCoreApplication, QFileInfo, Qt, QSettings, QTranslator) 
 from PyQt5.QtWidgets import (QAction, QApplication)
-from .htmldialog import HtmlDialog
 
 from qgis.core import (QgsMessageLog)
+from qgis.utils import showPluginHelp
 
 from .nominatim_dlg import nominatim_dlg
 from .nominatim_conf_dlg import nominatim_conf_dlg
 
-class nominatim: 
+
+class nominatim:
 
     def __init__(self, iface):
         # Save reference to the QGIS interface
@@ -45,32 +46,32 @@ class nominatim:
         self.gnUsername = ""
         self.defaultArea = Qt.LeftDockWidgetArea
         self.singleLayer = True
-        
+
         self.read()
-        
+
         # récup langue par défaut
         locale = QSettings().value("locale/userLocale")
         self.myLocale = locale[0:2]
         # exploiter le bon dictionnaire
-        localePath = QFileInfo(os.path.realpath(__file__)).path() + "/i18n/nominatim_" + self.myLocale + ".qm"
+        localePath = QFileInfo(os.path.realpath(__file__)).path() + "/i18n/nominatim_"+ self.myLocale + ".qm"
         # initialiser le traducteur
         if QFileInfo(localePath).exists():
             self.translator = QTranslator()
             self.translator.load(localePath)
             QCoreApplication.installTranslator(self.translator)
-                
+
         try:
             self.nominatim_dlg
         except:
             self.nominatim_dlg = nominatim_dlg(self.iface.mainWindow(), self)
             self.nominatim_dlg.visibilityChanged.connect(self.dockVisibilityChanged)
             self.nominatim_dlg.dockLocationChanged.connect(self.dockLocationChanged)
-        
+
         try:
             self.nominatim_dlg.editSearch.setText(self.lastSearch)
         except:
             pass
-                
+
     def store(self):
         s = QSettings()
         s.setValue("nominatim/localiseOnStartup", self.localiseOnStartup)
@@ -82,10 +83,10 @@ class nominatim:
         s.setValue("nominatim/gnUsername", self.gnUsername)
         s.setValue("nominatim/defaultArea", self.defaultArea)
         s.setValue("nominatim/singleLayer", self.singleLayer)
-    
+
     def read(self):
         s = QSettings()
-    
+
         self.localiseOnStartup = s.value("nominatim/localiseOnStartup", (False), type=bool)
         self.limitSearchToExtent = s.value("nominatim/limitSearchToExtent", (False), type=bool)
         self.dlgPosX = s.value("nominatim/dlgPosX", 100, type=int)
@@ -95,35 +96,35 @@ class nominatim:
         self.gnUsername = s.value("nominatim/gnUsername", '')
         self.defaultArea = s.value("nominatim/defaultArea", Qt.LeftDockWidgetArea, type=int)
         self.singleLayer = s.value("nominatim/singleLayer", (True), type=bool)
-    
+
     def initGui(self):  
         self.toolBar = self.iface.pluginToolBar()
-    
+
         self.act_config = QAction(QApplication.translate("nominatim", "Configuration", None) + "...", self.iface.mainWindow())
         self.act_nominatim_help = QAction(QApplication.translate("nominatim", "Help", None) + "...", self.iface.mainWindow())
-    
+
         self.iface.addPluginToMenu("&"+QApplication.translate("nominatim", "OSM place search", None) + "...", self.act_config)
         self.iface.addPluginToMenu("&"+QApplication.translate("nominatim", "OSM place search", None) + "...", self.act_nominatim_help)
-    
+
         # Add actions to the toolbar
         self.act_config.triggered.connect(self.do_config)
         self.act_nominatim_help.triggered.connect(self.do_help)
-        
+
         self.iface.addDockWidget( self.defaultArea, self.nominatim_dlg )
-        
+
     def unload(self):
         self.iface.removePluginMenu("&"+QApplication.translate("nominatim", "OSM place search", None) + "...", self.act_config)
         self.iface.removePluginMenu("&"+QApplication.translate("nominatim", "OSM place search", None) + "...", self.act_nominatim_help)
         self.store()
         self.deactivate()
         self.iface.removeDockWidget(self.nominatim_dlg)     
-    
+
     def dockVisibilityChanged(self, visible):
         try:
             self.defaultActive = visible
         except:
             pass
-    
+
     def dockLocationChanged(self, area):
         self.defaultArea = area
 
@@ -135,19 +136,17 @@ class nominatim:
             self.nominatim_dlg.hide()
         except:
             pass
-	
+
     def zoom(self):
         pass
-	
+
     def do_config(self):
         dlg = nominatim_conf_dlg(self.iface.mainWindow(), self)
         dlg.setModal(True)
-        
+
         dlg.show()
         dlg.exec_()
         del dlg
-    
+
     def do_help(self):
-        dlg = HtmlDialog( None, "help.html" )
-        dlg.exec_()
-            
+        showPluginHelp()
